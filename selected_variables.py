5c3593857cfd40b3161b16dd2e7a786fc557f192
Sneha Chetani <112269656+snehachetani@users.noreply.github.com>
2025-05-04 18:43:34 +0200

    Update selected_variables.py

--- selected_variables.py
+++ selected_variables.py
@@ -1,25 +1,41 @@
+def preprocess_data(ma1p, txwrdp):
+    ma1p['WORD'] = ma1p['WORD'].astype(str)
+    print('length of negative wdlp value: ', len(ma1p[(ma1p['WDLP'] == -1) | (ma1p['WDLP'] == -2)]))
 
+    ma1p = ma1p.drop(ma1p[(ma1p['WDLP'] == -1) | (ma1p['WDLP'] == -2)].index)
+    #ma1p['WDLP'] = ma1p['WDLP'].replace([-1, -2], 0)
+    #ma1p['WDLP'] = ma1p['WDLP'].replace(0, 1)
+    #ma1p['OBLP'] = ma1p['OBLP'].replace(0, 1)
 
-def preprocess_data(result_df, result_df2):
-    #result_df = result_df[:7430]
-    words = result_df['WORD']
-    wnum = result_df["WNUM"]
-    wlen = result_df["WLEN"]
-    xpos = result_df['XPOS']
-    wdlp = list(result_df['WDLP'])
-    oblp = list(result_df['OBLP'])
-    fdur = result_df['FDUR']
-    WORDs = result_df2['WORD']
-    FXNO = result_df2['FXNO']
-    TXFR  = result_df2['TXFR']
-    WNUM = result_df2['WNUM']
-    Participant_ID =  result_df2['Participant ID'] 
-    Text_File = result_df2["Text File"]
-    participant_id =  result_df['Participant ID'] 
-    text_file = result_df["Text File"]
+    print('length of empty string: ', len(ma1p[ma1p['WORD'] == '']))
+    ma1p = ma1p[ma1p['WORD'] != '']
+    #ma1p['WORD'] = ma1p['WORD'].replace('', ' ')
+
+    ma1p.reset_index(drop=True, inplace=True)
+    print('length after removing empty strings and negative wdlp words: ', len(ma1p))
+
     words_to_ignore = ['*Off-screen', '*Blink']
-    nan_words = result_df[result_df['WORD'].isna()]
-    words.fillna('0', inplace=True)
-    WORDs.fillna('0', inplace=True)
-    words_without_nan = result_df.dropna(subset=['WORD'])
-    return words, wnum, wlen, xpos, wdlp, oblp, fdur, words_to_ignore, participant_id, text_file, WORDs, FXNO, TXFR, WNUM, Participant_ID, Text_File
+
+    words_ma1 = ma1p["WORD"]
+    text_ma1 = ma1p["TEXT"]
+    line_ma1 = ma1p["LINE"]
+    olen_ma1 = ma1p['OLEN']
+    wlen_ma1 = ma1p['WLEN']
+    xpos_ma1 = ma1p['XPOS']
+    wnum_ma1 = ma1p['WNUM']
+    fdur_ma1 = ma1p['FDUR']
+    oblp_ma1 = ma1p['OBLP']
+    wdlp_ma1 = ma1p['WDLP']
+    laun_ma1 = ma1p["LAUN"]
+    txfr_ma1 = ma1p["TXFR"]
+
+    words_txw = txwrdp["WORD"]
+    text_txw = txwrdp["TEXT"]
+    line_txw = txwrdp["LINE"]
+    olen_txw = txwrdp['OLEN']
+    wlen_txw = txwrdp['WLEN']
+    wnum_txw = txwrdp['WNUM']
+    txfr_txw = txwrdp["TXFR"]
+    pos_screen_txw  = txwrdp['POS_Screen']
+
+    return words_ma1, text_ma1, line_ma1, olen_ma1, wlen_ma1, xpos_ma1, wnum_ma1, fdur_ma1, oblp_ma1, wdlp_ma1, laun_ma1, txfr_ma1, words_txw, text_txw, line_txw, olen_txw, wlen_txw, wnum_txw, txfr_txw, pos_screen_txw, words_to_ignore
